1188c8a2ea375a37ead9a21208fdb1eb681be9f0
Salem Fenn <salemlfenn@gmail.com>
2023-04-22 14:20:39 -0700

    Removed old imports in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,4 @@
-import { Redirect, Route } from "react-router-dom";
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact,
-} from "@ionic/react";
-import Tab1 from "./pages/Tab1";
-import Tab2 from "./pages/Tab2";
-import Tab3 from "./pages/Tab3";
+import { IonApp, setupIonicReact } from "@ionic/react";
 import { AuthProvider } from "./contexts/AuthContext";
 import Router from "./navigation/Router";
 
@@ -35,43 +22,6 @@ import "@ionic/react/css/display.css";
 import "./theme/variables.css";
 
 setupIonicReact();
-
-// const App: React.FC = () => (
-//   <IonApp>
-//     <IonReactRouter>
-//       <IonTabs>
-//         <IonRouterOutlet>
-//           <Route exact path="/tab1">
-//             <Tab1 />
-//           </Route>
-//           <Route exact path="/tab2">
-//             <Tab2 />
-//           </Route>
-//           <Route path="/tab3">
-//             <Tab3 />
-//           </Route>
-//           <Route exact path="/">
-//             <Redirect to="/tab1" />
-//           </Route>
-//         </IonRouterOutlet>
-//         <IonTabBar slot="bottom">
-//           <IonTabButton tab="tab1" href="/tab1">
-//             <IonIcon aria-hidden="true" icon={triangle} />
-//             <IonLabel>Tab 1</IonLabel>
-//           </IonTabButton>
-//           <IonTabButton tab="tab2" href="/tab2">
-//             <IonIcon aria-hidden="true" icon={ellipse} />
-//             <IonLabel>Tab 2</IonLabel>
-//           </IonTabButton>
-//           <IonTabButton tab="tab3" href="/tab3">
-//             <IonIcon aria-hidden="true" icon={square} />
-//             <IonLabel>Tab 3</IonLabel>
-//           </IonTabButton>
-//         </IonTabBar>
-//       </IonTabs>
-//     </IonReactRouter>
-//   </IonApp>
-// );
 
 const App: React.FC = () => (
   <IonApp>
