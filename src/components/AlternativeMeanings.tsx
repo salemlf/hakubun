7e59cf5960de05577abb51f0d285d088565cadcc
Salem Fenn <salemlfenn@gmail.com>
2023-05-15 20:52:48 -0700

    Displaying placeholder if no alt meanings

--- src/components/AlternativeMeanings.tsx
+++ src/components/AlternativeMeanings.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { IonRow } from "@ionic/react";
 
 import { SubjAndAssignment } from "../types/MiscTypes";
@@ -5,23 +6,33 @@ import { SubjAndAssignment } from "../types/MiscTypes";
 import { getAlternativeMeanings } from "../services/SubjectAndAssignmentService";
 
 import styles from "./AlternativeMeanings.module.scss";
+import { SubjectMeaning } from "../types/Subject";
 
 type Props = {
   subject: SubjAndAssignment;
 };
-// TODO: display "-" if no alternative meanings
 
 //   TODO: add a button to add alternative meanings/synonyms
 export const AlternativeMeanings = ({ subject }: Props) => {
+  const [altMeanings, setAltMeanings] = useState<SubjectMeaning[]>([]);
+
+  useEffect(() => {
+    let alt = getAlternativeMeanings(subject);
+
+    setAltMeanings(alt);
+  }, [subject]);
+
   return (
     <IonRow className={`${styles.altMeaningsTxt}`}>
       <h2>Alternative Meanings</h2>
       <p>
-        {(getAlternativeMeanings(subject) as [])
-          .map((altMeaning: any) => {
-            return altMeaning.meaning;
-          })
-          .join(", ")}
+        {altMeanings.length
+          ? altMeanings
+              .map((altMeaning: any) => {
+                return altMeaning.meaning;
+              })
+              .join(", ")
+          : "-"}
       </p>
       <IonRow />
     </IonRow>
