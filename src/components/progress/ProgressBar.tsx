5b236e35ef320e34d08cf2766b09e96a3db55bb3
Salem Fenn <salemlfenn@gmail.com>
2023-07-09 12:10:13 -0700

    Using styled components now in progress bar instead of standard scss

--- src/components/progress/ProgressBar.tsx
+++ src/components/progress/ProgressBar.tsx
@@ -4,12 +4,55 @@ import { IonSkeletonText } from "@ionic/react";
 import { useKanjiAssignmentsForLvl } from "../../hooks/useKanjiAssignmentsForLvl";
 
 import { getAssignmentStatuses } from "../../services/SubjectAndAssignmentService";
+import styled from "styled-components/macro";
 
-import styles from "./ProgressBar.module.scss";
+const ProgressContainer = styled.div`
+  width: 100%;
+  border: 2px solid var(--wanikani-pink);
+  margin: 10px 0;
+  border-radius: 10px;
+`;
 
-interface Props {
+const Backdrop = styled.div`
+  position: relative;
+  background-color: var(--wanikani-pink);
+  border-radius: 10px;
+
+  .source {
+    mix-blend-mode: screen;
+  }
+
+  .contents,
+  .bar {
+    height: 20px;
+  }
+`;
+
+const BarBg = styled.div`
+  background-color: white;
+  border-radius: 10px;
+`;
+
+const Bar = styled.div`
+  background-color: black;
+  border-radius: 10px;
+`;
+
+const Contents = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  mix-blend-mode: difference;
+`;
+
+type Props = {
   level: number | undefined;
-}
+};
 
 export const ProgressBar = ({ level }: Props) => {
   const barRef = useRef<null | HTMLDivElement>(null);
@@ -39,19 +82,18 @@ export const ProgressBar = ({ level }: Props) => {
   return (
     <>
       {!kanjiAssignmentsLvlLoading && (
-        <div className={`${styles.container}`}>
-          <div className={`${styles.backdrop}`}>
-            <div className={`${styles.source}`}>
-              <div className={`${styles.barBg}`}>
-                <div ref={barRef} className={`${styles.bar}`}></div>
-              </div>
-              <div className={`${styles.contents}`}>
+        <ProgressContainer>
+          <Backdrop>
+            <div className="source">
+              <BarBg>
+                <Bar ref={barRef} className="bar"></Bar>
+              </BarBg>
+              <Contents className="contents">
                 <span>{completedTxt}</span>
-              </div>
-              <div></div>
+              </Contents>
             </div>
-          </div>
-        </div>
+          </Backdrop>
+        </ProgressContainer>
       )}
       {kanjiAssignmentsLvlLoading && (
         <div>
