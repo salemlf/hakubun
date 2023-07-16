ba9b903845696865b00351514448caceeec56f76
Salem Fenn <salemlfenn@gmail.com>
2023-07-16 11:13:23 -0700

    Button now adds user meaning to subject details page

--- src/components/subjects/AddUserMeaningButton.tsx
+++ src/components/subjects/AddUserMeaningButton.tsx
@@ -1,8 +1,16 @@
-import { IonAlert, IonButton, IonChip, IonIcon, IonLabel } from "@ionic/react";
+import {
+  IonAlert,
+  IonChip,
+  IonIcon,
+  IonLabel,
+  IonSkeletonText,
+} from "@ionic/react";
 import { addOutline } from "ionicons/icons";
 import { Subject } from "../../types/Subject";
 import { useStudyMaterialsBySubjIDs } from "../../hooks/useStudyMaterialsBySubjIDs";
 import styled from "styled-components/macro";
+import { useUpdateStudyMaterials } from "../../hooks/misc/useUpdateStudyMaterials";
+import { updateMeaningSynonymsInStudyMaterial } from "../../services/MiscService";
 
 type Props = {
   subject: Subject;
@@ -32,51 +40,100 @@ export const AddUserMeaningButton = ({ subject }: Props) => {
     error: studyMaterialErr,
   } = useStudyMaterialsBySubjIDs([subject.id]);
 
+  const { mutate: updateStudyMaterials } = useUpdateStudyMaterials();
+
+  const addUserMeaning = (userMeaningToAdd: string) => {
+    console.log(
+      "🚀 ~ file: AddUserMeaningButton.tsx:102 ~ addUserMeaning ~ studyMaterialData:",
+      studyMaterialData
+    );
+    if (studyMaterialData) {
+      //
+      let updatedMaterialsWithAddedMeaning =
+        updateMeaningSynonymsInStudyMaterial(
+          studyMaterialData,
+          userMeaningToAdd,
+          "add"
+        );
+      // *testing
+      console.log(
+        "🚀 ~ file: AddUserMeaningButton.tsx:107 ~ addUserMeaning ~ updatedMaterialsWithAddedMeaning:",
+        updatedMaterialsWithAddedMeaning
+      );
+      // *testing
+      // TODO: data already exists, call updateStudyMaterialsMutation (do put request using study materials ID)
+
+      updateStudyMaterials({
+        studyMaterialID: studyMaterialData.id,
+        updatedStudyMaterials: updatedMaterialsWithAddedMeaning,
+      });
+    } else {
+      // TODO: data doesn't exist, call constructStudyMaterialData and then separate function to put data (do post using subject id to create)
+      console.log("NOT IMPLEMENTED - Need to construct data");
+    }
+  };
+
   return (
     <>
-      <AddChip
-        id="present-user-meaning-add"
-        onClick={(e: any) => {
-          console.log("ADD ITEM CLICKED! ");
-        }}
-      >
-        <IonLabel>Add</IonLabel>
-        <IonIcon icon={addOutline}></IonIcon>
-      </AddChip>
-      <Alert
-        trigger="present-user-meaning-add"
-        header="Add Meaning"
-        buttons={[
-          {
-            text: "Cancel",
-            role: "cancel",
-            handler: () => {
-              // *testing
-              console.log("Canceled user meaning add");
-              // *testing
-            },
-          },
-          {
-            text: "Add",
-            role: "add",
-            handler: () => {
-              // *testing
-              console.log("Adding meaning...");
-              // *testing
-              // TODO: call function to add meaning
-            },
-          },
-        ]}
-        inputs={[
-          {
-            label: "Meaning",
-            type: "textarea",
-            attributes: {
-              maxlength: 100,
-            },
-          },
-        ]}
-      ></Alert>
+      {!studyMaterialLoading ? (
+        <>
+          {" "}
+          <AddChip
+            id="present-user-meaning-add"
+            onClick={(e: any) => {
+              console.log("ADD ITEM CLICKED! ");
+            }}
+          >
+            <IonLabel>Add</IonLabel>
+            <IonIcon icon={addOutline}></IonIcon>
+          </AddChip>
+          <Alert
+            trigger="present-user-meaning-add"
+            header="Add Meaning"
+            buttons={[
+              {
+                text: "Cancel",
+                role: "cancel",
+                handler: () => {
+                  // *testing
+                  console.log("Canceled user meaning add");
+                  // *testing
+                },
+              },
+              {
+                text: "Add",
+                role: "add",
+                handler: (alertData) => {
+                  // *testing
+                  console.log(
+                    "🚀 ~ file: AddUserMeaningButton.tsx:85 ~ AddUserMeaningButton ~ alertData:",
+                    alertData
+                  );
+                  console.log("Adding meaning...");
+                  // *testing
+                  // TODO: call function to add meaning
+                  addUserMeaning(alertData.meaning);
+                },
+              },
+            ]}
+            inputs={[
+              {
+                name: "meaning",
+                label: "Meaning",
+                type: "textarea",
+                attributes: {
+                  maxlength: 100,
+                },
+              },
+            ]}
+          ></Alert>
+        </>
+      ) : (
+        <IonSkeletonText
+          animated={true}
+          style={{ height: "20px" }}
+        ></IonSkeletonText>
+      )}
     </>
   );
 };
