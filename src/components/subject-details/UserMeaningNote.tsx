91ceb6a24b97c885643009c72c06ee2fb2aaf748
Salem Fenn <salemlfenn@gmail.com>
2023-07-18 17:59:31 -0700

    Showing buttons for saving, editing, canceling, and deleting note. Focusing on note when edit button is clicked

--- src/components/subject-details/UserMeaningNote.tsx
+++ src/components/subject-details/UserMeaningNote.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { IonIcon, IonSkeletonText } from "@ionic/react";
 import NoteIcon from "../../images/note.svg";
 import PencilIcon from "../../images/pencil.svg";
+import TrashIcon from "../../images/trash.svg";
+import SaveIcon from "../../images/save.svg";
+import CancelIcon from "../../images/cancel.svg";
 import { Subject } from "../../types/Subject";
 import { useStudyMaterialsBySubjIDs } from "../../hooks/useStudyMaterialsBySubjIDs";
 import { StudyMaterialDataResponse } from "../../types/MiscTypes";
@@ -12,6 +15,11 @@ import {
   IconHeadingContainer,
 } from "../styles/BaseStyledComponents";
 
+const NoteContainer = styled(NoteHintContainer)`
+  position: relative;
+  padding-bottom: 15px;
+`;
+
 const NoteIconStyled = styled(IonIcon)`
   margin-right: 5px;
   vertical-align: text-top;
@@ -20,46 +28,109 @@ const NoteIconStyled = styled(IonIcon)`
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
+  position: absolute;
+  right: 5px;
+  bottom: -16px;
 `;
 
 const EditingButton = styled.button`
-  padding: 5px;
+  padding: 7px;
   border-radius: 50%;
   box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
 
   ion-icon {
     width: 1.25rem;
     height: 1.25rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
+`;
+
+const TrashButton = styled(EditingButton)`
+  background-color: var(--ion-color-danger-tint);
+  color: white;
+  margin-right: 15px;
 `;
 
 const PencilButton = styled(EditingButton)`
   background-color: var(--ion-color-primary);
+  color: white;
+`;
+
+const SaveButton = styled(EditingButton)`
+  background-color: var(--ion-color-tertiary);
   color: black;
+`;
+
+const CancelButton = styled(EditingButton)`
+  background-color: var(--ion-color-warning);
+  color: black;
+  margin-right: 15px;
+`;
+
+const Textarea = styled.textarea`
+  resize: none;
+  width: 100%;
+  background-color: var(--light-grey);
 `;
 
 type NoteProps = {
   studyMaterialsResponse: StudyMaterialDataResponse;
 };
 
+// TODO: on cancel button click -> save without changes
+// TODO: on trash button click -> confirm note deletion
 const Note = ({ studyMaterialsResponse }: NoteProps) => {
   const [isEditable, setIsEditable] = useState(false);
 
+  const startEditing = () => {
+    setIsEditable(true);
+  };
+
   return (
-    <NoteHintContainer>
+    <NoteContainer>
       <IconHeadingContainer>
         <NoteIconStyled src={NoteIcon} />
         <NoteHintHeading>Meaning Note</NoteHintHeading>
       </IconHeadingContainer>
-      <div contentEditable={isEditable}>
-        {studyMaterialsResponse.meaning_note}
-      </div>
+      {isEditable ? (
+        <Textarea
+          ref={(ref) => ref && ref.focus()}
+          onFocus={(e) =>
+            e.currentTarget.setSelectionRange(
+              e.currentTarget.value.length,
+              e.currentTarget.value.length
+            )
+          }
+          defaultValue={studyMaterialsResponse.meaning_note!}
+        ></Textarea>
+      ) : (
+        <div>{studyMaterialsResponse.meaning_note}</div>
+      )}
+
       <ButtonContainer>
-        <PencilButton>
-          <IonIcon src={PencilIcon} />
-        </PencilButton>
+        {isEditable ? (
+          <>
+            <CancelButton aria-label="Cancel">
+              <IonIcon src={CancelIcon} />
+            </CancelButton>
+            <SaveButton aria-label="Save">
+              <IonIcon src={SaveIcon} onClick={() => setIsEditable(false)} />
+            </SaveButton>
+          </>
+        ) : (
+          <>
+            <TrashButton aria-label="Delete">
+              <IonIcon src={TrashIcon} />
+            </TrashButton>
+            <PencilButton onClick={() => startEditing()} aria-label="Edit">
+              <IonIcon src={PencilIcon} />
+            </PencilButton>
+          </>
+        )}
       </ButtonContainer>
-    </NoteHintContainer>
+    </NoteContainer>
   );
 };
 
