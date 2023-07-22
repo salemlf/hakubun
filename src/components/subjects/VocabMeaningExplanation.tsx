b98247a8e3927f151f5ef5f379d936ef43f029d5
Salem Fenn <salemlfenn@gmail.com>
2023-07-21 21:45:01 -0700

    Meaning note component added to vocab page

--- src/components/subjects/VocabMeaningExplanation.tsx
+++ src/components/subjects/VocabMeaningExplanation.tsx
@@ -4,7 +4,8 @@ import {
 } from "../subject-details/SubjectDetailsStyled";
 import { TxtWithSubjTags } from "../TxtWithSubjTags";
 import { Hint } from "../subject-details/Hint";
-import { Vocabulary } from "../../types/Subject";
+import { Subject, Vocabulary } from "../../types/Subject";
+import { UserMeaningNote } from "../subject-details/UserMeaningNote";
 
 type Props = {
   vocab: Vocabulary;
@@ -16,6 +17,7 @@ export const VocabMeaningExplanation = ({ vocab }: Props) => {
       <SubjDetailSubHeading>Meaning Explanation</SubjDetailSubHeading>
       <TxtWithSubjTags textWithTags={vocab.meaning_mnemonic} />
       {vocab.reading_hint && <Hint hint={vocab.reading_hint} />}
+      <UserMeaningNote subject={vocab as Subject} />
     </SubjDetailSection>
   );
 };
