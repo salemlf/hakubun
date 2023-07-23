1e98ad17f41fc1b527684a461b4d2eef0aa3a6c2
Salem Fenn <salemlfenn@gmail.com>
2023-07-22 21:56:58 -0700

    Added meaning icon to vocab meaning explanation

--- src/components/subjects/VocabMeaningExplanation.tsx
+++ src/components/subjects/VocabMeaningExplanation.tsx
@@ -6,6 +6,9 @@ import { TxtWithSubjTags } from "../TxtWithSubjTags";
 import { Hint } from "../subject-details/Hint";
 import { Subject, Vocabulary } from "../../types/Subject";
 import { UserNote } from "../subject-details/UserNote";
+import { IconHeadingContainer } from "../styles/BaseStyledComponents";
+import MeaningIcon from "../../images/meaning.svg";
+import { IonIcon } from "@ionic/react";
 
 type Props = {
   vocab: Vocabulary;
@@ -14,7 +17,10 @@ type Props = {
 export const VocabMeaningExplanation = ({ vocab }: Props) => {
   return (
     <SubjDetailSection>
-      <SubjDetailSubHeading>Meaning Explanation</SubjDetailSubHeading>
+      <IconHeadingContainer>
+        <IonIcon src={MeaningIcon} />
+        <SubjDetailSubHeading>Meaning Explanation</SubjDetailSubHeading>
+      </IconHeadingContainer>
       <TxtWithSubjTags textWithTags={vocab.meaning_mnemonic} />
       {vocab.meaning_hint && <Hint hint={vocab.meaning_hint} />}
       <UserNote subject={vocab as Subject} noteType="meaning" />
