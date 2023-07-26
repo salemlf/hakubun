f80a74d1afe4bbdf25f9c6d0ba78c68e2164ddba
Salem Fenn <salemlfenn@gmail.com>
2023-07-26 16:40:31 -0700

    useAllSubjects hook now using useInfiniteQuery and paging data. Also modified so data is cached longer and not refetched on window focus

--- src/hooks/useAllSubjects.ts
+++ src/hooks/useAllSubjects.ts
@@ -1,17 +1,27 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { WaniKaniAPI } from "../api/WaniKaniApi";
-import { flattenData } from "../services/MiscService";
+import { flattenPagesOfData } from "../services/MiscService";
 
 export const useAllSubjects = () => {
-  return useQuery({
-    queryKey: ["all-subjects"],
-    queryFn: () => WaniKaniAPI.getAllSubjects(),
-    select: (data: any) => {
-      return flattenData(data);
+  return useInfiniteQuery(
+    ["all-subjects"],
+    ({ pageParam }) => {
+      return WaniKaniAPI.getAllSubjects(pageParam);
     },
-    // stale time of 6 hours
-    staleTime: 6 * 60 * (60 * 1000),
-    // cache time of 7 hours
-    cacheTime: 7 * 60 * (60 * 1000),
-  });
+    {
+      getNextPageParam: (lastPage) => {
+        return lastPage.data.length !== 0 ? lastPage.pages.next_url : null;
+      },
+      select: (data: any) => {
+        let flattenedPages = flattenPagesOfData(data);
+        return flattenedPages;
+      },
+      keepPreviousData: true,
+      // stale time of 8 hours
+      staleTime: 8 * 60 * (60 * 1000),
+      // cache time of 9 hours
+      cacheTime: 9 * 60 * (60 * 1000),
+      refetchOnWindowFocus: false,
+    }
+  );
 };
