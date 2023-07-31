14e43818d9a8fcde498a2f33ba209c3ec0f2426f
Salem Fenn <salemlfenn@gmail.com>
2023-07-31 12:20:17 -0700

    Temporarily commenting out review summary page contents while restructuring contents, submitting completed reviews

--- src/components/ReviewSummary/ReviewSummary.tsx
+++ src/components/ReviewSummary/ReviewSummary.tsx
@@ -20,6 +20,12 @@ import {
 import GroupedReviewSummaryResults from "./GroupedReviewSummaryResults";
 import { FullWidthGrid } from "../../styles/BaseStyledComponents";
 import styled from "styled-components/macro";
+import { useEffect, useState } from "react";
+import {
+  ReviewPostItem,
+  ReviewQueueItem,
+} from "../../types/ReviewSessionTypes";
+import { useCreateReview } from "./useCreateReview";
 
 const Page = styled(IonPage)`
   --ion-background-color: var(--light-greyish-purple);
@@ -81,63 +87,105 @@ const CardContent = styled(IonCardContent)`
 `;
 
 function ReviewSummary() {
-  const { queueDataState } = useReviewQueue();
-  let reviewQueue = queueDataState.reviewQueue;
-  // *testing
-  console.log(
-    "🚀 ~ file: ReviewSummary.tsx:7 ~ ReviewSummary ~ reviewQueue:",
-    reviewQueue
+  const [reviewQueueItems, setReviewQueueItems] = useState<ReviewQueueItem[]>(
+    []
   );
-  // *testing
-  let reviewData = getCompletedReviewSessionData(reviewQueue);
-  // !added
-  console.log(
-    "🚀 ~ file: ReviewSummary.tsx:92 ~ ReviewSummary ~ reviewData:",
-    reviewData
-  );
-  let reviewPostDataArr = createReviewPostData(reviewData);
-  console.log(
-    "🚀 ~ file: ReviewSummary.tsx:99 ~ ReviewSummary ~ reviewPostDataArr:",
-    reviewPostDataArr
-  );
-  // !added
-  let reviewsByResult = getReviewsGroupedByResult(reviewData);
-  let numCorrect = reviewsByResult.correct.length;
-  let numWrong = reviewsByResult.incorrect.length;
-  let percentageCorrect = Math.ceil(100 * (numCorrect / reviewData.length));
+  const { mutate: createReviews } = useCreateReview();
+  // TODO: change so not using "any" type
+  const [reviewPostDataArr, setReviewPostDataArr] = useState<any>([]);
+  const { endReviewSession, queueDataState } = useReviewQueue();
+  useEffect(() => {
+    // *testing
+    console.log("Running Review summary useEffect!");
+    // *testing
 
-  let correctSubjIDs = reviewsByResult.correct.map(
-    (reviewItem: any) => reviewItem.id
-  );
-  let incorrectSubjIDs = reviewsByResult.incorrect.map(
-    (reviewItem: any) => reviewItem.id
-  );
+    let reviewQueue = queueDataState.reviewQueue;
+    // TODO: call
+    let reviewData = getCompletedReviewSessionData(reviewQueue);
+    setReviewQueueItems(reviewData);
 
-  let hasCorrect = correctSubjIDs.length !== 0;
-  let hasIncorrect = incorrectSubjIDs.length !== 0;
+    let reviewPostData = createReviewPostData(reviewData);
+    setReviewPostDataArr(reviewPostData);
 
-  const {
-    isLoading: correctReviewSubjLoading,
-    data: correctReviewSubjData,
-    error: correctReviewSubjErr,
-  } = useSubjectsByIDs(correctSubjIDs, hasCorrect);
+    // *testing
+    console.log(
+      "🚀 ~ file: ReviewSummary.tsx:121 ~ useEffect ~ reviewPostData:",
+      reviewPostData
+    );
+    // *testing
 
-  const {
-    isLoading: incorrectReviewSubjLoading,
-    data: incorrectReviewSubjData,
-    error: incorrectReviewSubjErr,
-  } = useSubjectsByIDs(incorrectSubjIDs, hasIncorrect);
+    submitReviews(reviewPostData);
 
-  let reviewSummaryDataLoading =
-    (correctReviewSubjLoading && hasCorrect) ||
-    (incorrectReviewSubjLoading && hasIncorrect);
+    endReviewSession();
+  }, []);
+
+  const submitReviews = (reviewPostData: ReviewPostItem[]) => {
+    let tempArr: any = [];
+
+    // TODO: take response and call setReviewPostDataArr with it so can be used for summary
+    reviewPostData.forEach((reviewItem: ReviewPostItem) => {
+      createReviews(
+        { reviewSessionData: reviewItem },
+        {
+          onSuccess: (data) => {
+            tempArr.push(data);
+          },
+        }
+      );
+    });
+
+    // *testing
+    console.log(
+      "🚀 ~ file: ReviewSummary.tsx:149 ~ submitReviews ~ tempArr:",
+      tempArr
+    );
+    // *testing
+  };
+
+  // let reviewsByResult = getReviewsGroupedByResult(reviewData);
+  // let numCorrect = reviewsByResult.correct.length;
+  // let numWrong = reviewsByResult.incorrect.length;
+  // let percentageCorrect = Math.ceil(100 * (numCorrect / reviewData.length));
+
+  // let reviewsByResult = getReviewsGroupedByResult(reviewQueueItems);
+  // let numCorrect = reviewsByResult.correct.length;
+  // let numWrong = reviewsByResult.incorrect.length;
+  // let percentageCorrect = Math.ceil(
+  //   100 * (numCorrect / reviewQueueItems.length)
+  // );
+
+  // let correctSubjIDs = reviewsByResult.correct.map(
+  //   (reviewItem: any) => reviewItem.id
+  // );
+  // let incorrectSubjIDs = reviewsByResult.incorrect.map(
+  //   (reviewItem: any) => reviewItem.id
+  // );
+
+  // let hasCorrect = correctSubjIDs.length !== 0;
+  // let hasIncorrect = incorrectSubjIDs.length !== 0;
+
+  // const {
+  //   isLoading: correctReviewSubjLoading,
+  //   data: correctReviewSubjData,
+  //   error: correctReviewSubjErr,
+  // } = useSubjectsByIDs(correctSubjIDs, hasCorrect);
+
+  // const {
+  //   isLoading: incorrectReviewSubjLoading,
+  //   data: incorrectReviewSubjData,
+  //   error: incorrectReviewSubjErr,
+  // } = useSubjectsByIDs(incorrectSubjIDs, hasIncorrect);
+
+  // let reviewSummaryDataLoading =
+  //   (correctReviewSubjLoading && hasCorrect) ||
+  //   (incorrectReviewSubjLoading && hasIncorrect);
 
   return (
     <Page>
       <Header>
         <IonToolbar>
           <Title>Review Summary</Title>
-          <Percentage>{percentageCorrect}%</Percentage>
+          {/* <Percentage>{percentageCorrect}%</Percentage> */}
           <p>Answered Correctly</p>
         </IonToolbar>
       </Header>
@@ -145,9 +193,9 @@ function ReviewSummary() {
         <FullWidthGrid>
           <IonCard>
             <IncorrectItemsHeader>
-              <CardTitle>{numWrong} Answered Incorrectly</CardTitle>
+              {/* <CardTitle>{numWrong} Answered Incorrectly</CardTitle> */}
             </IncorrectItemsHeader>
-            {!reviewSummaryDataLoading ? (
+            {/* {!reviewSummaryDataLoading ? (
               <CardContent>
                 {incorrectReviewSubjData && (
                   <GroupedReviewSummaryResults
@@ -183,7 +231,7 @@ function ReviewSummary() {
                   style={{ height: "50px" }}
                 ></IonSkeletonText>
               </CardContent>
-            )}
+            )} */}
           </IonCard>
         </FullWidthGrid>
       </IonContent>
