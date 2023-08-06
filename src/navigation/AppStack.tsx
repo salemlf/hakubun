d4b6a2289ef1850fb5f6e0cccaa7a2ec8889c18a
Salem Fenn <salemlfenn@gmail.com>
2023-08-05 17:03:02 -0700

    Simplified AppStack since not placing tabs in component

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -15,56 +15,25 @@ import ReviewSummary from "../pages/ReviewSummary";
 export const AppStack = () => {
   return (
     <Router>
-      <Tabs />
+      <Switch>
+        <Route
+          path="/review/settings"
+          component={ReviewSettings}
+          exact={true}
+        />
+        <Route
+          path="/review/session"
+          component={ReviewSessionQueue}
+          exact={true}
+        />
+        <Route path="/review/summary" component={ReviewSummary} exact={true} />
+        <Route path="/subjects" component={Subjects} exact={true} />
+        <Route path="/search" component={Search} exact={true} />
+        <Route path="/subjects/:id" component={SubjectDetails} />
+        <Route path="/home" component={Home} exact={true} />
+        <Route exact={true} path="/" render={() => <Redirect to="/home" />} />
+        <Redirect from="/authenticate" to="/home" exact={true} />
+      </Switch>
     </Router>
-  );
-};
-
-const TabsStyled = styled(IonTabs)`
-  ion-tab-bar {
-    box-shadow: 0px 1px 8px rgba(0, 0, 0, 0.4);
-    border-radius: 30px;
-    width: 65%;
-    min-width: 200px;
-    max-width: 500px;
-    padding-top: 5px;
-    padding-bottom: 5px;
-    padding-left: 10px;
-    padding-right: 10px;
-
-    bottom: 20px;
-    position: relative;
-    margin: 0 auto;
-    border-top: none;
-    gap: 5px;
-  }
-
-  ion-tab-button {
-    border-radius: 30px;
-
-    &:focus {
-      outline: 2px solid white;
-      --outline: 2px solid white;
-    }
-  }
-`;
-
-export const Tabs = () => {
-  return (
-    <Switch>
-      <Route path="/review/settings" component={ReviewSettings} exact={true} />
-      <Route
-        path="/review/session"
-        component={ReviewSessionQueue}
-        exact={true}
-      />
-      <Route path="/review/summary" component={ReviewSummary} exact={true} />
-      <Route path="/subjects" component={Subjects} exact={true} />
-      <Route path="/search" component={Search} exact={true} />
-      <Route path="/subjects/:id" component={SubjectDetails} />
-      <Route path="/home" component={Home} exact={true} />
-      <Route exact={true} path="/" render={() => <Redirect to="/home" />} />
-      <Redirect from="/authenticate" to="/home" exact={true} />
-    </Switch>
   );
 };
