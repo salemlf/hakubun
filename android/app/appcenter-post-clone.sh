c0ac399d02c63ca7feb1899d046aab1993f3e101
Salem Fenn <salemlfenn@gmail.com>
2023-08-14 19:59:26 -0700

    Added post clone scripts for appcenter to use in ios and android

---
+++ android/app/appcenter-post-clone.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+# Required nodeJS version
+NODE_VERSION=20.5.0
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node20 "$NODE_VERSION"
+
+# go to root of project
+cd ../..
+
+# install dependencies
+npm i
+
+# run optimized production build
+npm run build -- --prod
+
+# copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
+npx cap sync
