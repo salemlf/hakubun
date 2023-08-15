9225f01ddc04c4bd35480ea47ebc769c461153a4
Salem Fenn <salemlfenn@gmail.com>
2023-08-15 07:27:22 -0700

    Commented out setting node version in android post clone script, trying simpler setup

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-# fail if any command fails
-set -e
-# debug log
-set -x
+# # fail if any command fails
+# set -e
+# # debug log
+# set -x
 
-# Required nodeJS version
-NODE_VERSION=20.5.0
+# # Required nodeJS version
+# NODE_VERSION=20.5.0
 
-# workaround to override the v8 alias
-npm config delete prefix
-. ~/.bashrc
-nvm install "$NODE_VERSION"
-nvm alias node20 "$NODE_VERSION"
+# # workaround to override the v8 alias
+# npm config delete prefix
+# . ~/.bashrc
+# nvm install "$NODE_VERSION"
+# nvm alias node20 "$NODE_VERSION"
+
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
 
 # substitute APP_SECRET_VALUE in App Center config file
 envsubst < ./src/main/assets/appcenter-config.json > ./src/main/assets/temp-appcenter-config.json && mv ./src/main/assets/temp-appcenter-config.json ./src/main/assets/appcenter-config.json
@@ -27,4 +31,4 @@ npm i
 npm run build
 
 # copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
-npx cap sync android
+npx cap sync
