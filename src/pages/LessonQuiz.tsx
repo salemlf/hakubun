b3e6946004c34f54734482364b3eec23daa8c8b9
Salem Fenn <salemlfenn@gmail.com>
2023-08-19 23:04:10 -0700

    Submitting llessons on lesson quiz completion, starts assignments and redirects to lesson  summary page

--- src/pages/LessonQuiz.tsx
+++ src/pages/LessonQuiz.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react";
 import { IonContent, IonGrid } from "@ionic/react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+// TODO: instead add a module declaration file for react-router-prompt
+// @ts-ignore: Could not find a declaration file for module
+import ReactRouterPrompt from "react-router-prompt";
 import { shuffleArray } from "../services/MiscService";
+import { useAssignmentQueueStore } from "../stores/useAssignmentQueueStore";
 import { ReviewQueueItem } from "../types/ReviewSessionTypes";
 import AnimatedPage from "../components/AnimatedPage";
 import ReviewCards from "../components/ReviewCards";
 import QueueHeader from "../components/QueueHeader";
 import styled from "styled-components";
-import { useAssignmentQueueStore } from "../stores/useAssignmentQueueStore";
+import { blockUserLeavingPage } from "../services/ReviewService";
+import { useQueueStore } from "../stores/useQueueStore";
+import Dialog from "../components/Dialog";
+import { useStartAssignment } from "../hooks/useStartAssignment";
 
 const Page = styled(AnimatedPage)`
   background-color: var(--dark-greyish-purple);
@@ -24,10 +31,14 @@ const Grid = styled(IonGrid)`
 // TODO: add ReactRouterPrompt and account for user trying to navigate away from page
 function LessonQuiz() {
   const location = useLocation();
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
+  const resetQueueStore = useQueueStore.use.resetAll();
+  const resetAssignmentQueue = useAssignmentQueueStore.use.resetAll();
   const assignmentQueue = useAssignmentQueueStore.use.assignmentQueue();
   const setAssignmentQueueData =
     useAssignmentQueueStore.use.setAssignmentQueueData();
+  const { mutateAsync: startAssignmentAsync } = useStartAssignment();
 
   let lessonQueueFromSession: ReviewQueueItem[] = location.state;
 
@@ -48,15 +59,78 @@ function LessonQuiz() {
     }
   }, [location.state]);
 
+  const endLessonQuiz = () => {
+    resetQueueStore();
+    resetAssignmentQueue();
+  };
+
   // TODO: start assignments and redirect to lesson summary page
   const submitLessonQuiz = (queueData: ReviewQueueItem[]) => {
     // *testing
     console.log("submitLessonQuiz NOT IMPLEMENTED YET");
     // *testing
+    // TODO: change to actually catch errors
+    let promises = queueData.map(function (lessonItem) {
+      return startAssignmentAsync({
+        assignmentID: lessonItem.assignment_id,
+      })
+        .then(function (results) {
+          // *testing
+          console.log("🚀 ~ file: LessonQuiz.tsx:77 ~ results:", results);
+          // *testing
+          return results;
+          // return results.resources_updated.assignment;
+        })
+        .catch((err) => {
+          // *testing
+          console.log(
+            "🚀 ~ file: ReviewSessionQueue.tsx:96 ~ promises ~ err:",
+            err
+          );
+          // *testing
+        });
+    });
+    Promise.all(promises).then(function (results) {
+      // *testing
+      console.log(results);
+      // *testing
+
+      let lessonInfo = {
+        lessonResponses: results,
+      };
+      navigate("/lessons/summary", { state: lessonInfo, replace: true });
+    });
   };
 
   return (
     <Page>
+      <ReactRouterPrompt
+        when={blockUserLeavingPage}
+        beforeConfirm={() => {
+          endLessonQuiz();
+        }}
+      >
+        {({
+          isActive,
+          onConfirm,
+          onCancel,
+        }: {
+          isActive: boolean;
+          onConfirm: () => void;
+          onCancel: () => void;
+        }) =>
+          isActive && (
+            <Dialog
+              uncontrolledSettings={{ defaultOpen: isActive }}
+              title="End review session?"
+              confirmText="End Session"
+              cancelText="Cancel"
+              onConfirmClick={onConfirm}
+              onCancelClick={onCancel}
+            />
+          )
+        }
+      </ReactRouterPrompt>
       {!isLoading && assignmentQueue.length !== 0 && <QueueHeader />}
       <IonContent>
         <Grid>
