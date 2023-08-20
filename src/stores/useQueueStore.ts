8d266304b3d9736b8ebaea48a00a25a6dfb937c0
Salem Fenn <salemlfenn@gmail.com>
2023-08-19 17:40:02 -0700

    Added additional functions and state to queue store to replace useReviewQueue, renamed to be more generic

---
+++ src/stores/useQueueStore.ts
@@ -0,0 +1,66 @@
+import { create } from "zustand";
+import { createSelectors } from "../utils";
+import { PopoverInfo } from "../types/ReviewSessionTypes";
+
+interface QueueState {
+  isSecondClick: boolean;
+  isBottomSheetVisible: boolean;
+  showRetryButton: boolean;
+  popoverInfo: PopoverInfo;
+  displayPopoverMsg: boolean;
+  savedUserAnswer: string | null;
+}
+
+interface QueueActions {
+  setSecondClick: (isSecondClick: boolean) => void;
+  retryReview: () => void;
+  showPopoverMsg: (popoverInfo: PopoverInfo) => void;
+  resetReviewCards: () => void;
+  setSavedUserAnswer: (savedUserAnswer: string | null) => void;
+  correctShowResult: () => void;
+  correctMoveToNext: () => void;
+  wrongMoveToNext: () => void;
+  wrongShowResult: () => void;
+  submitChoice: () => void;
+}
+
+const useQueueStoreBase = create<QueueState & QueueActions>((set, get) => ({
+  isSecondClick: false,
+  isBottomSheetVisible: false,
+  showRetryButton: false,
+  popoverInfo: { message: "", messageType: "invalid" },
+  displayPopoverMsg: false,
+  savedUserAnswer: null,
+  setSecondClick: (isSecondClick) => set({ isSecondClick }),
+  retryReview: () =>
+    set({
+      isSecondClick: false,
+      displayPopoverMsg: false,
+      isBottomSheetVisible: false,
+      showRetryButton: false,
+    }),
+  showPopoverMsg: (state) =>
+    set({ popoverInfo: { ...state }, displayPopoverMsg: true }),
+  resetReviewCards: () =>
+    set({
+      displayPopoverMsg: false,
+      isSecondClick: false,
+      isBottomSheetVisible: false,
+      showRetryButton: false,
+    }),
+  setSavedUserAnswer: (savedUserAnswer) => set({ savedUserAnswer }),
+  correctShowResult: () => set({ isBottomSheetVisible: true }),
+  correctMoveToNext: () =>
+    set({ isBottomSheetVisible: false, displayPopoverMsg: false }),
+  wrongMoveToNext: () =>
+    set({
+      isBottomSheetVisible: false,
+      displayPopoverMsg: false,
+      showRetryButton: false,
+    }),
+  wrongShowResult: () =>
+    set({ isBottomSheetVisible: true, showRetryButton: true }),
+  submitChoice: () => set({ isSecondClick: !get().isSecondClick }),
+}));
+
+export const useQueueStore = createSelectors(useQueueStoreBase);
