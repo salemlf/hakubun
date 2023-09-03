d23f6e3934e8d982a2c952b633d36338c7e8ac6d
Salem Fenn <salemlfenn@gmail.com>
2023-09-03 14:54:18 -0700

    Moved usePrevious hook into its own file

---
+++ src/hooks/usePrevious.ts
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+
+// cred to Nadia Makarevich, tutorial here: https://www.developerway.com/posts/implementing-advanced-use-previous-hook
+export const usePrevious = <TValue extends unknown>(value: TValue) => {
+  const ref = useRef<{ value: TValue; prev: TValue | null }>({
+    value: value,
+    prev: null,
+  });
+
+  const current = ref.current.value;
+
+  if (value !== current) {
+    ref.current = {
+      value: value,
+      prev: current,
+    };
+  }
+
+  return ref.current.prev;
+};
