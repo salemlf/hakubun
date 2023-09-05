c29f208d66c815c2ed613fd4a942cead9f66ba4a
Salem Fenn <salemlfenn@gmail.com>
2023-09-05 13:08:55 -0700

    Header with back button displayed for user settings, started creating cards to section out different kinds of settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,19 +1,47 @@
 import { IonContent } from "@ionic/react";
 import AnimatedPage from "../components/AnimatedPage";
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { Header } from "../styles/BaseStyledComponents";
+import BackButton from "../components/BackButton";
+import Card from "../components/Card";
 
-// const Page = styled(AnimatedPage)`
-//   background-color: var(--dark-greyish-purple);
-// `;
+const SettingsHeader = styled(Header)`
+  padding: 14px 5px;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  align-items: center;
+`;
+
+const PageHeading = styled.h1`
+  margin: 0;
+  margin-right: 16px;
+  text-align: center;
+`;
+
+const SettingCategory = styled(Card)`
+  display: flex;
+`;
 
 function Settings() {
-  const navigate = useNavigate();
-
   return (
     <AnimatedPage>
+      <SettingsHeader bgcolor="var(--light-greyish-purple)">
+        <BackButton backgroundColor="var(--ion-color-secondary)" />
+        <PageHeading>User Settings</PageHeading>
+      </SettingsHeader>
       <IonContent>
-        <p>TODO: settings page</p>
+        <SettingCategory
+          title="General"
+          headerBgColor="var(--ion-color-primary)"
+        ></SettingCategory>
+        <SettingCategory
+          title="Lessons"
+          headerBgColor="var(--wanikani-lesson)"
+        ></SettingCategory>
+        <SettingCategory
+          title="Reviews"
+          headerBgColor="var(--wanikani-review)"
+        ></SettingCategory>
       </IonContent>
     </AnimatedPage>
   );
