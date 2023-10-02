ad389af21a784cf99b94ae68c844c8f21442fa8e
Salem Fenn <salemlfenn@gmail.com>
2023-10-01 19:48:04 -0700

    Updated app version to 0.0.7-alpha

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ import "./theme/globals.scss";
 // TODO: improve this so not manually changing release version every time
 if (import.meta.env.MODE !== "development") {
   LogRocket.init("cleqvf/hakubun", {
-    release: "0.0.6-alpha",
+    release: "0.0.7-alpha",
     shouldCaptureIP: false,
   });
 }
