008812dea88c0535aa288374e0ed6513f4fae818
Salem Fenn <salemlfenn@gmail.com>
2023-10-02 12:52:49 -0700

    Deleted unused useHideOnKeyboardOpen hook

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,7 +3,6 @@ import { IonList, IonSearchbar } from "@ionic/react";
 import Fuse from "fuse.js";
 import { flattenSearchResults } from "../services/MiscService";
 import { useAllSubjects } from "../hooks/useAllSubjects";
-import { useHideOnKeyboardOpen } from "../hooks/useHideOnKeyboardOpen";
 import { SubjectWideButton } from "../components/SubjectWideBtnList";
 import AnimatedPage from "../components/AnimatedPage/AnimatedPage";
 import FloatingTabBar from "../components/FloatingTabBar";
@@ -57,7 +56,6 @@ const LogoSearchOutcomeContainer = styled(AbsoluteCenterContainer)`
 export const Search = () => {
   let [results, setResults] = useState<Fuse.FuseResult<unknown>[]>([]);
   const [query, setQuery] = useState("");
-  // const { shouldHide } = useHideOnKeyboardOpen();
 
   const options = {
     threshold: 0.1,
