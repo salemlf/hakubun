055b2846891b80b15f0f9608a49d77e400d37699
Salem Fenn <salemlfenn@gmail.com>
2023-10-07 11:28:20 -0700

    Collapsible component now animated on open and close

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -4,6 +4,7 @@ import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
 import CollapseArrowColorIcon from "../../images/collapse-arrow-color.svg";
 import ExpandArrowColorIcon from "../../images/expand-arrow-color.svg";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const TriggerWithTitle = styled.div`
   display: flex;
@@ -38,6 +39,42 @@ const TriggerButton = styled.button`
   }
 `;
 
+const Content = styled(CollapsiblePrimitive.Content)`
+  overflow: hidden;
+`;
+
+const collapsibleContentVariants = {
+  open: {
+    height: "auto",
+    opacity: 1,
+    display: "block",
+    transition: {
+      height: {
+        duration: 0.4,
+      },
+      opacity: {
+        duration: 0.25,
+        delay: 0.15,
+      },
+    },
+  },
+  closed: {
+    height: 0,
+    opacity: 0,
+    transition: {
+      height: {
+        duration: 0.4,
+      },
+      opacity: {
+        duration: 0.25,
+      },
+    },
+    transitionEnd: {
+      display: "none",
+    },
+  },
+};
+
 type Props = {
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
@@ -46,7 +83,6 @@ type Props = {
   titleFontSize?: string;
 };
 
-// TODO: add animation
 function Collapsible({
   isOpen,
   setIsOpen,
@@ -55,18 +91,11 @@ function Collapsible({
   titleFontSize = "1.25rem",
 }: Props) {
   return (
-    <CollapsiblePrimitive.Root
-      className="CollapsibleRoot"
-      open={isOpen}
-      onOpenChange={setIsOpen}
-    >
+    <CollapsiblePrimitive.Root open={isOpen} onOpenChange={setIsOpen}>
       <TriggerWithTitle>
-        <Title className="Text" titlefontsize={titleFontSize}>
-          {title}
-        </Title>
+        <Title titlefontsize={titleFontSize}>{title}</Title>
         <CollapsiblePrimitive.Trigger asChild>
           <TriggerButton
-            className="IconButton"
             aria-label={isOpen ? "Collapse content" : "Expand content"}
           >
             <CollapseOrExpandIcon
@@ -75,7 +104,15 @@ function Collapsible({
           </TriggerButton>
         </CollapsiblePrimitive.Trigger>
       </TriggerWithTitle>
-      <CollapsiblePrimitive.Content>{children}</CollapsiblePrimitive.Content>
+
+      <Content
+        as={motion.div}
+        initial={false}
+        variants={collapsibleContentVariants}
+        animate={isOpen ? "open" : "closed"}
+      >
+        {children}
+      </Content>
     </CollapsiblePrimitive.Root>
   );
 }
