b211f0eaf5755fbfbc584ab8a2af6a7ba18644f5
Salem Fenn <salemlfenn@gmail.com>
2023-10-07 10:26:40 -0700

    Available assignment types now being passed into advanced assignment settings

--- src/components/AdvancedAssignmentSettings/AdvancedAssignmentSettings.tsx
+++ src/components/AdvancedAssignmentSettings/AdvancedAssignmentSettings.tsx
@@ -1,28 +1,46 @@
-import { Assignment } from "../../types/Assignment";
+import { useState } from "react";
+import { Assignment, AssignmentType } from "../../types/Assignment";
+import { AssignmentTypeName } from "../AssignmentTypeSelector/AssignmentTypeSelector.types";
 import Card from "../Card";
 import AssignmentSelector from "../AssignmentSelector";
+import AdvancedAssignmentFilters from "../AdvancedAssignmentFilters";
 
 type Props = {
   assignmentData: Assignment[];
   showMeaning: boolean;
   selectedAdvancedSubjIDs: string[];
   setSelectedAdvancedSubjIDs: React.Dispatch<React.SetStateAction<string[]>>;
+  availableAssignmentTypes: AssignmentType[];
+  availableAssignmentTypeNames: AssignmentTypeName[];
 };
 
-// TODO: move assignment selector into this file?
+// TODO: make sure to deselect items if not in selectedAssignmentTypes
 function AdvancedAssignmentSettings({
   assignmentData,
   selectedAdvancedSubjIDs,
   setSelectedAdvancedSubjIDs,
   showMeaning,
+  availableAssignmentTypes,
+  availableAssignmentTypeNames,
 }: Props) {
+  // TODO: filter based on selected types
+  const [selectedAssignmentTypes, setSelectedAssignmentTypes] = useState(
+    availableAssignmentTypes
+  );
+
   return (
     <Card>
+      <AdvancedAssignmentFilters
+        availableAssignmentTypeNames={availableAssignmentTypeNames}
+        selectedAssignmentTypes={selectedAssignmentTypes}
+        setSelectedAssignmentTypes={setSelectedAssignmentTypes}
+      />
       <AssignmentSelector
         selectedAdvancedSubjIDs={selectedAdvancedSubjIDs}
         setSelectedAdvancedSubjIDs={setSelectedAdvancedSubjIDs}
         showMeaning={showMeaning}
         assignmentData={assignmentData}
+        assignmentFilters={selectedAssignmentTypes}
       ></AssignmentSelector>
     </Card>
   );
