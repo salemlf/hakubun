1967415cb7e2ea0f9b9e6f6a5715e79fee8b22cb
Salem Fenn <salemlfenn@gmail.com>
2023-10-27 14:53:20 -0700

    Created a couple tests for create review hook

--- src/hooks/useCreateReview.test.ts
+++ src/hooks/useCreateReview.test.ts
@@ -1,25 +1,25 @@
-// import { waitFor, renderHook } from "@testing-library/react";
-// import { rest } from "msw";
-// import { server } from "../testing/mocks/server";
-// import { createWrapper } from "../testing/test-utils";
-// import { useCreateReview } from "./useCreateReview";
-// import { baseUrl } from "../api/ApiConfig";
+import { rest } from "msw";
+import { renderHook, waitFor, createWrapper } from "../testing/test-utils";
+import { server } from "../testing/mocks/server";
+import { baseUrl } from "../api/ApiConfig";
+import { useCreateReview } from "./useCreateReview";
 
-// describe("useCreateReview", () => {
-//   it("Tests responses when creating reviews", async () => {
-//     const reviewsEndpoint = `${baseUrl}reviews`;
-//     server.use(
-//       rest.post(reviewsEndpoint, (req, res, ctx) => {
-//         return res(ctx.status(429), ctx.json([]));
-//       })
-//     );
+// TODO: test that attempt is made to resubmit review if it fails
+describe("useCreateReview", () => {
+  it("Tests responses when creating reviews", async () => {
+    const reviewsEndpoint = `${baseUrl}reviews`;
+    server.use(
+      rest.post(reviewsEndpoint, (req, res, ctx) => {
+        return res(ctx.status(429), ctx.json([]));
+      })
+    );
 
-//     test("Returns a too many requests error when submitting reviews", async () => {
-//       const { result } = renderHook(() => useCreateReview(), {
-//         wrapper: createWrapper(),
-//       });
-//       await waitFor(() => expect(result.current.isError).toBe(true));
-//       await waitFor(() => expect(result.current.status).toBe(429));
-//     });
-//   });
-// });
+    test("Returns a too many requests error when submitting reviews", async () => {
+      const { result } = renderHook(() => useCreateReview(), {
+        wrapper: createWrapper(),
+      });
+      await waitFor(() => expect(result.current.isError).toBe(true));
+      await waitFor(() => expect(result.current.status).toBe(429));
+    });
+  });
+});
