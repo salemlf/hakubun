ec4efcf04650971871197ed35e3443a776e29c0d
Salem Fenn <salemlfenn@gmail.com>
2023-10-29 14:56:01 -0700

    Mock queue items updated to include starting_srs_stage, created test for updateSubmittedQueueItems

--- src/stores/useAssignmentSubmitStore.test.ts
+++ src/stores/useAssignmentSubmitStore.test.ts
@@ -1,3 +1,4 @@
+import { mockAssignmentQueueItems } from "../testing/mocks/data/assignmentQueueItems.mock";
 import { act, renderHook } from "../testing/test-utils";
 import { useAssignmentSubmitStore } from "./useAssignmentSubmitStore";
 
@@ -5,9 +6,30 @@ describe("useAssignmentSubmitStore", () => {
   it("Initial store values", () => {
     test("Initial values are as expected", () => {
       const { result } = renderHook(() => useAssignmentSubmitStore());
-      expect(result.current.submittedAssignmentResponses).toEqual([]);
       expect(result.current.submittedAssignmentsWithErrs).toEqual([]);
       expect(result.current.shouldBatchSubmit).toEqual(false);
+    });
+  });
+
+  it("updateSubmittedQueueItems", () => {
+    test("Submitted queue items added", () => {
+      const { result } = renderHook(() => useAssignmentSubmitStore());
+      // make sure nothing in queue
+      act(() => result.current.resetAll());
+      expect(result.current.submittedAssignmentQueueItems).toEqual([]);
+
+      let halfMockArrLength = mockAssignmentQueueItems.length / 2;
+      const firstHalf = mockAssignmentQueueItems.slice(0, halfMockArrLength);
+      const secondHalf = mockAssignmentQueueItems.slice(halfMockArrLength + 1);
+      // combining arrs instead of using mockAssignmentQueueItems directly in case length of mock items is ever 1
+      const allMockItems = [...firstHalf, ...secondHalf];
+
+      act(() => result.current.updateSubmittedQueueItems(firstHalf));
+      expect(result.current.submittedAssignmentQueueItems).toEqual(firstHalf);
+      act(() => result.current.updateSubmittedQueueItems(secondHalf));
+      expect(result.current.submittedAssignmentQueueItems).toEqual(
+        allMockItems
+      );
     });
   });
 });
