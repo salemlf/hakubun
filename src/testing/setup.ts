e112f07e0ffbeb5d67d1a3f5ef1936ae092b9ba2
Salem Fenn <salemlfenn@gmail.com>
2023-10-29 17:06:15 -0700

    Setup updated to call ionic app creation and modified create functions for zustand mocking, now the tests actually are accurate lol

--- src/testing/setup.ts
+++ src/testing/setup.ts
@@ -1,20 +1,35 @@
+import { setupIonicReact } from "@ionic/react";
 import { expect, afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { server } from "./mocks/server";
+
+setupIonicReact();
+
+// Mock matchmedia
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
 
 vi.mock("zustand"); // to make it work like Jest (auto-mocking)
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
 
+beforeAll(() => server.listen());
+
 // clean up after each test case
 afterEach(() => {
   server.resetHandlers();
   cleanup();
 });
-
-beforeAll(() => server.listen());
 
 // clean up after the tests are finished.
 afterAll(() => server.close());
