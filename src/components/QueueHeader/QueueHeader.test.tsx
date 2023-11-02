7806eaf084b0488a232af1a755ff9d25eca695e2
Salem Fenn <salemlfenn@gmail.com>
2023-11-02 13:22:49 -0700

    Created basic queue header test

---
+++ src/components/QueueHeader/QueueHeader.test.tsx
@@ -0,0 +1,11 @@
+import QueueHeader from ".";
+import { renderWithRouter } from "../../testing/test-utils";
+
+test("QueueHeader renders", () => {
+  const { baseElement } = renderComponent();
+  expect(baseElement).toBeDefined();
+});
+
+const renderComponent = () => {
+  return renderWithRouter(<QueueHeader />);
+};
