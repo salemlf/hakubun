3f181ed997c0d0a7a61c5f1b1cd64a64557fe2c8
Salem Fenn <salemlfenn@gmail.com>
2023-11-02 17:40:18 -0700

    Created Only and Either type defs

--- src/types/Global.ts
+++ src/types/Global.ts
@@ -5,3 +5,11 @@ export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
   {
     [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
   }[Keys];
+
+// cred: https://stackoverflow.com/a/66605669
+export type Only<T, U> = { [P in keyof T]: T[P] } & Omit<
+  { [P in keyof U]?: never },
+  keyof T
+>;
+
+export type Either<T, U> = Only<T, U> | Only<U, T>;
