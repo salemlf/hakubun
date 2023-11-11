8d127b22a590bd449eff1b6abeb4aa9215e175d8
Salem Fenn <salemlfenn@gmail.com>
2023-11-11 12:27:18 -0800

    Black border added to subject buttons

--- src/styles/SubjectButtonsStyled.tsx
+++ src/styles/SubjectButtonsStyled.tsx
@@ -21,10 +21,7 @@ export const DefaultBtn = styled.button<DefaultBtnProps>`
     filter: brightness(0) invert(1);
   }
 
-  --box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12);
+  border: 2px solid black;
 `;
 
 type TxtBtnProps = {
