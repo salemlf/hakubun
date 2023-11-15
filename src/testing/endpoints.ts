c177882db6a3a6a7eb3e666591826c741339d1c1
Salem Fenn <salemlfenn@gmail.com>
2023-11-14 18:10:53 -0800

    Updated avail for review endpoint

--- src/testing/endpoints.ts
+++ src/testing/endpoints.ts
@@ -3,5 +3,5 @@ import { baseUrl } from "./../api/ApiConfig";
 export const userEndpoint = `${baseUrl}user`;
 export const assignmentsEndpoint = `${baseUrl}assignments`;
 export const subjectsEndpoint = `${baseUrl}subjects`;
-export const assignmentsAvailForReviewEndpoint = `${baseUrl}assignments?immediately_available_for_review`;
+export const assignmentsAvailForReviewEndpoint = `${baseUrl}assignments?immediately_available_for_review=true`;
 export const assignmentsAvailForLessonsEndpoint = `${baseUrl}assignments?immediately_available_for_lessons=true`;
