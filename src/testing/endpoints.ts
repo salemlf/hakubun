775413a9630d97bd9c3fed69a9078a1532e68593
Salem Fenn <salemlfenn@gmail.com>
2023-11-15 17:30:32 -0800

    Endpoints include param constants

--- src/testing/endpoints.ts
+++ src/testing/endpoints.ts
@@ -5,3 +5,11 @@ export const assignmentsEndpoint = `${baseUrl}assignments`;
 export const subjectsEndpoint = `${baseUrl}subjects`;
 export const assignmentsAvailForReviewEndpoint = `${baseUrl}assignments?immediately_available_for_review=true`;
 export const assignmentsAvailForLessonsEndpoint = `${baseUrl}assignments?immediately_available_for_lessons=true`;
+
+export const AVAIL_REVIEWS = "immediately_available_for_review=true";
+export const AVAIL_LESSONS = "immediately_available_for_lessons=true";
+
+export const SUBJECT_SUBJ_TYPES = "types";
+export const ASSIGNMENT_SUBJ_TYPES = "subject_types";
+export const LEVELS_PARAM = "levels";
+export const SRS_STAGES = "srs_stages";
