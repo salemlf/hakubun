c22ea26d5411762d7dfa439e4d9d80839284cf26
Salem Fenn <salemlfenn@gmail.com>
2023-11-16 18:15:09 -0800

    Mock error responses created

---
+++ src/testing/mocks/data/errResponses.mock.ts
@@ -0,0 +1,10 @@
+export const perms401Err = {
+  error:
+    "The personal access token does not grant permission to access this endpoint",
+  code: 401,
+};
+
+export const unauthorized401 = {
+  status: 401,
+  statusText: "Unauthorized",
+};
