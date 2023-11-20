37441e4fd4fa774e76a3c01262042b9b6f1d136d
Salem Fenn <salemlfenn@gmail.com>
2023-11-19 16:12:49 -0800

    Now asks user to confirm if they want to leave lesson session

--- src/pages/LessonSession.tsx
+++ src/pages/LessonSession.tsx
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react";
 // TODO: change so not relying on IonIcon
 import { IonIcon } from "@ionic/react";
 import { useNavigate } from "react-router-dom";
-import { useAssignmentQueueStore } from "../stores/useAssignmentQueueStore/useAssignmentQueueStore";
+// TODO: instead add a module declaration file for react-router-prompt
+// @ts-ignore: Could not find a declaration file for module
+import ReactRouterPrompt from "react-router-prompt";
+import useQueueStoreFacade from "../stores/useQueueStore/useQueueStore.facade";
 import useLessonPaginatorStoreFacade from "../stores/useLessonPaginatorStore/useLessonPaginatorStore.facade";
+import useAssignmentQueueStoreFacade from "../stores/useAssignmentQueueStore/useAssignmentQueueStore.facade";
+import { blockUserLeavingPage } from "../services/AssignmentQueueService";
 import { AssignmentQueueItem } from "../types/AssignmentQueueTypes";
 import LessonCards from "../components/LessonCards";
 import Button from "../components/Button";
+import AlertModal from "../components/AlertModal";
 import HomeIconColor from "../images/home-color.svg";
 import styled from "styled-components";
 
@@ -27,10 +33,14 @@ const HomeIconStyled = styled(IonIcon)`
 
 function LessonSession() {
   const navigate = useNavigate();
-  const lessonQueue = useAssignmentQueueStore((state) => state.assignmentQueue);
+  // const lessonQueue = useAssignmentQueueStore((state) => state.assignmentQueue);
   const [uniqueLessonQueue, setUniqueLessonQueue] = useState<
     AssignmentQueueItem[]
   >([]);
+  const { resetAll: resetQueueStore } = useQueueStoreFacade();
+  const { resetAll: resetAssignmentQueue, assignmentQueue: lessonQueue } =
+    useAssignmentQueueStoreFacade();
+
   const { reset: resetLessonPaginator } = useLessonPaginatorStoreFacade();
 
   useEffect(() => {
@@ -50,10 +60,45 @@ function LessonSession() {
     resetLessonPaginator();
   };
 
+  const endLessonSession = () => {
+    resetQueueStore();
+    resetAssignmentQueue();
+  };
+
   return (
     <>
       {uniqueLessonQueue.length !== 0 && (
         <>
+          <ReactRouterPrompt
+            when={blockUserLeavingPage}
+            beforeConfirm={() => {
+              endLessonSession();
+            }}
+          >
+            {({
+              isActive,
+              onConfirm,
+              onCancel,
+            }: {
+              isActive: boolean;
+              onConfirm: () => void;
+              onCancel: () => void;
+            }) =>
+              isActive && (
+                <AlertModal open={isActive}>
+                  <AlertModal.Content
+                    isOpen={isActive}
+                    title="End Lesson Session?"
+                    confirmText="End Session"
+                    description="Are you sure you want to leave? You'll lose all progress from this lesson session."
+                    cancelText="Cancel"
+                    onConfirmClick={onConfirm}
+                    onCancelClick={onCancel}
+                  />
+                </AlertModal>
+              )
+            }
+          </ReactRouterPrompt>
           <HomeBtn onPress={() => navigate("/", { replace: true })}>
             <HomeIconStyled icon={HomeIconColor}></HomeIconStyled>
           </HomeBtn>
