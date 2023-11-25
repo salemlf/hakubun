1dcd05e374ca44816b7a6ec348ceb17255103a2b
Salem Fenn <salemlfenn@gmail.com>
2023-11-25 11:12:12 -0800

    Updated remaining files referencing study materials types

--- src/components/AssignmentSettings/AssignmentSettings.tsx
+++ src/components/AssignmentSettings/AssignmentSettings.tsx
@@ -20,10 +20,11 @@ import {
   MAX_ASSIGNMENTS_BEFORE_SUBMIT,
 } from "../../constants";
 import { Assignment } from "../../types/Assignment";
-import { AssignmentBatch, StudyMaterial } from "../../types/MiscTypes";
+import { AssignmentBatch } from "../../types/MiscTypes";
 import { AssignmentSessionType } from "../../types/AssignmentQueueTypes";
 import { BackToBackChoice } from "../BackToBackOption/BackToBackOption.types";
 import { Subject, SubjectType } from "../../types/Subject";
+import { StudyMaterial } from "../../types/StudyMaterial";
 import BasicAssignmentSettings from "../BasicAssignmentSettings";
 import SwipeableTabs from "../SwipeableTabs";
 import AdvancedAssignmentSettings from "../AdvancedAssignmentSettings";
--- src/testing/mocks/data-generators/assignmentQueueGenerator.ts
+++ src/testing/mocks/data-generators/assignmentQueueGenerator.ts
@@ -2,8 +2,8 @@ import { createAssignmentQueueItems } from "../../../services/SubjectAndAssignme
 import { BackToBackChoice } from "../../../components/BackToBackOption/BackToBackOption.types";
 import { Assignment } from "../../../types/Assignment";
 import { AssignmentQueueItem } from "../../../types/AssignmentQueueTypes";
-import { StudyMaterial } from "../../../types/MiscTypes";
 import { Subject } from "../../../types/Subject";
+import { StudyMaterial } from "../../../types/StudyMaterial";
 
 // TODO: use areCompleted to generate completed items
 export const mockQueueItems = (
