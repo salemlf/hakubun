a04cfcec1f20a9355a26a4bc041af7edf1a24a59
Salem Fenn <salemlfenn@gmail.com>
2024-03-14 17:59:39 -0700

    Incremented app version to 0.3.8-alpha

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ import "@ionic/react/css/display.css";
 import "./theme/variables.css";
 import "./theme/globals.scss";
 
-const releaseVersion = "0.3.7-alpha";
+const releaseVersion = "0.3.8-alpha";
 
 // for mock service worker
 async function enableMocking() {
