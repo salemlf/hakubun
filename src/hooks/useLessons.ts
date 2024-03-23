d72fbb34cc73ece2f8c6b4069c8d1ca5a8244ddb
Salem Fenn <salemlfenn@gmail.com>
2024-03-23 11:58:58 -0700

    Typed useLessons response and added basic test for successful response

--- src/hooks/useLessons.ts
+++ src/hooks/useLessons.ts
@@ -1,12 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { WaniKaniAPI } from "../api/WaniKaniApi";
 import { flattenData } from "../services/MiscService/MiscService";
+import { Assignment } from "../types/Assignment";
 
 // TODO: increase time to wait between data fetches
 export const useLessons = () => {
   return useQuery({
     queryKey: ["available-lessons"],
     queryFn: WaniKaniAPI.getLessons,
-    select: (data) => flattenData(data),
+    select: (pagedData) => {
+      const flattenedData: Assignment[] = flattenData(pagedData.data, false);
+      return flattenedData;
+    },
   });
 };
