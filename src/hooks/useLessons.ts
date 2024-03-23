e8c7849b9262bcb248bd031e50783552508229e3
Salem Fenn <salemlfenn@gmail.com>
2024-03-23 12:01:08 -0700

    Stale time and garbage collection time increased for useLessons

--- src/hooks/useLessons.ts
+++ src/hooks/useLessons.ts
@@ -3,7 +3,6 @@ import { WaniKaniAPI } from "../api/WaniKaniApi";
 import { flattenData } from "../services/MiscService/MiscService";
 import { Assignment } from "../types/Assignment";
 
-// TODO: increase time to wait between data fetches
 export const useLessons = () => {
   return useQuery({
     queryKey: ["available-lessons"],
@@ -12,5 +11,9 @@ export const useLessons = () => {
       const flattenedData: Assignment[] = flattenData(pagedData.data, false);
       return flattenedData;
     },
+    // stale time of 20 minutes
+    staleTime: 20 * (60 * 1000),
+    // garbage collection time of 30 minutes
+    gcTime: 30 * (60 * 1000),
   });
 };
