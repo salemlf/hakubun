00ff41aacf37889f8ee47c6d2c9c8227f4e8a8d8
Salem Fenn <salemlfenn@gmail.com>
2024-03-23 13:48:52 -0700

    Function to create mock response for assignments of specific type added

--- src/testing/mocks/api-responses/assignment-responses.ts
+++ src/testing/mocks/api-responses/assignment-responses.ts
@@ -1,11 +1,13 @@
 import { HttpResponse, http, passthrough } from "msw";
 import { server } from "../server";
 import {
+  ASSIGNMENT_SUBJ_TYPES,
   AVAIL_LESSONS,
   AVAIL_REVIEWS,
   assignmentsEndpoint,
 } from "../../endpoints";
 import { AssignmentCollection } from "../../../types/Collection";
+import { SubjectType } from "../../../types/Subject";
 
 export const mockLessonsAvailableResponse = (
   assignmentCollection: AssignmentCollection
@@ -30,6 +32,22 @@ export const mockReviewsAvailableResponse = (
       const url = new URL(request.url);
       const availReviews = url.searchParams.get(AVAIL_REVIEWS);
       if (availReviews) {
+        return HttpResponse.json(assignmentCollection);
+      }
+      return passthrough();
+    })
+  );
+};
+
+export const mockAssignmentsOfSubjTypeResponse = (
+  assignmentCollection: AssignmentCollection,
+  subjType: SubjectType
+) => {
+  server.use(
+    http.get(assignmentsEndpoint, ({ request }) => {
+      const url = new URL(request.url);
+      const assignmentSubjTypes = url.searchParams.get(ASSIGNMENT_SUBJ_TYPES);
+      if (assignmentSubjTypes === subjType) {
         return HttpResponse.json(assignmentCollection);
       }
       return passthrough();
