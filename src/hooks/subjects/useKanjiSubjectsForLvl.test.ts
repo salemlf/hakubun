b2e49f89bd3ca43c65fe834d26ae468362c4f8c1
Salem Fenn <salemlfenn@gmail.com>
2024-03-24 16:12:18 -0700

    Created basic test for useKanjiSubjectsForLvl query

---
+++ src/hooks/subjects/useKanjiSubjectsForLvl.test.ts
@@ -0,0 +1,15 @@
+import { createWrapper, renderHook, waitFor } from "../../testing/test-utils";
+import { generateSubjectCollection } from "../../testing/mocks/data-generators/collectionGenerator";
+import { mockSubjsOfTypeForLvlResponse } from "../../testing/mocks/api-responses/subject-responses";
+import { useKanjiSubjectsForLvl } from "./useKanjiSubjectsForLvl";
+
+test("useKanjiSubjectsForLvl gets successful response", async () => {
+  const mockLvl = 1;
+  const mockKanjiSubjCollection = generateSubjectCollection("kanji", 20);
+  mockSubjsOfTypeForLvlResponse(mockKanjiSubjCollection, "kanji");
+  const { result } = renderHook(() => useKanjiSubjectsForLvl(mockLvl), {
+    wrapper: createWrapper(),
+  });
+
+  await waitFor(() => expect(result.current.isSuccess).toBe(true));
+});
