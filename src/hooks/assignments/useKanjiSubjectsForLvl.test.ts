68818b0e8c1b8ae8690dfdab65594610f3898a6d
Salem Fenn <salemlfenn@gmail.com>
2024-03-24 17:50:26 -0700

    mockKanjiSubjsForLvlResponse function removed, using more generic mockSubjsOfTypeForLvlResponse

--- src/hooks/assignments/useKanjiSubjectsForLvl.test.ts
+++ src/hooks/assignments/useKanjiSubjectsForLvl.test.ts
@@ -1,12 +1,12 @@
 import { createWrapper, renderHook, waitFor } from "../../testing/test-utils";
 import { generateSubjectCollection } from "../../testing/mocks/data-generators/collectionGenerator";
-import { mockKanjiSubjsForLvlResponse } from "../../testing/mocks/api-responses/subject-responses";
+import { mockSubjsOfTypeForLvlResponse } from "../../testing/mocks/api-responses/subject-responses";
 import { useKanjiSubjectsForLvl } from "../subjects/useKanjiSubjectsForLvl";
 
 test("useKanjiSubjectsForLvl gets successful response", async () => {
   const mockLvl = 1;
   const mockKanjiSubjCollection = generateSubjectCollection(20, "kanji");
-  mockKanjiSubjsForLvlResponse(mockKanjiSubjCollection);
+  mockSubjsOfTypeForLvlResponse(mockKanjiSubjCollection, "kanji");
   const { result } = renderHook(() => useKanjiSubjectsForLvl(mockLvl), {
     wrapper: createWrapper(),
   });
--- src/testing/mocks/api-responses/subject-responses.ts
+++ src/testing/mocks/api-responses/subject-responses.ts
@@ -4,22 +4,6 @@ import { subjectsEndpoint } from "../../endpoints";
 import { SubjectCollection } from "../../../types/Collection";
 import { SubjectType } from "../../../types/Subject";
 
-export const mockKanjiSubjsForLvlResponse = (
-  subjCollection: SubjectCollection
-) => {
-  server.use(
-    http.get(subjectsEndpoint, ({ request }) => {
-      const url = new URL(request.url);
-      const levels = url.searchParams.get("levels");
-      const subjTypes = url.searchParams.get("types");
-      if (levels && subjTypes === "kanji") {
-        return HttpResponse.json(subjCollection);
-      }
-      return passthrough();
-    })
-  );
-};
-
 export const mockSubjsOfTypeForLvlResponse = (
   subjCollection: SubjectCollection,
   subjType: SubjectType
