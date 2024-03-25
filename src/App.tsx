5c393a097f7cb202a292bf8eb4eb4adbc67e9980
Salem Fenn <salemlfenn@gmail.com>
2024-03-24 18:33:15 -0700

    Commented out ReactQueryDevtools import to avoid annoying warning

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { baseUrlRegex, setAxiosHeaders } from "./api/ApiConfig";
 import { routes } from "./navigation/routes";
 import useAuthTokenStoreFacade from "./stores/useAuthTokenStore/useAuthTokenStore.facade";
