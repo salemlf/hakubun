908840fc332c29de11ca7f4f9ded7a2c2f40c2c5
Salem Fenn <salemlfenn@gmail.com>
2024-04-10 17:05:39 -0700

    Normal and alert modals using useModalContainer hook, modal styles moved into file so not repeating across components

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -86,7 +86,6 @@ export const ModalContent = forwardRef<ContentRef, ContentProps>(
                     initial={{ scale: 0 }}
                     animate={{ scale: 1 }}
                     exit={{ scale: 0 }}
-                    style={{ position: "absolute", zIndex: 5000 }}
                   >
                     <TitleBar>
                       <Title>{title}</Title>
