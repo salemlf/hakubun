bd98cdc9abeac6146c51ee19e259719a8cc78921
Salem Fenn <salemlfenn@gmail.com>
2024-04-13 16:09:09 -0700

    Query param constant for subject IDs added

--- src/testing/endpoints.ts
+++ src/testing/endpoints.ts
@@ -6,10 +6,11 @@ export const subjectsEndpoint = `${baseUrl}subjects`;
 export const studyMaterialsEndpoint = `${baseUrl}study_materials`;
 export const reviewsEndpoint = `${baseUrl}reviews`;
 
+// query params
 export const AVAIL_REVIEWS = "immediately_available_for_review";
 export const AVAIL_LESSONS = "immediately_available_for_lessons";
-
 export const SUBJECT_SUBJ_TYPES = "types";
 export const ASSIGNMENT_SUBJ_TYPES = "subject_types";
 export const LEVELS_PARAM = "levels";
 export const SRS_STAGES = "srs_stages";
+export const SUBJECT_IDS = "subject_ids";
