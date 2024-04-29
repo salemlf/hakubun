bedaad01aefde802402cce66f851419bd2ea30e2
Salem Fenn <salemlfenn@gmail.com>
2024-04-28 18:04:55 -0700

    Properly displaying keyboard shortcuts on review session page

--- src/pages/ReviewSession.tsx
+++ src/pages/ReviewSession.tsx
@@ -25,7 +25,7 @@ import styled from "styled-components";
 const Content = styled(MainContent)`
   height: 100svh;
   display: grid;
-  grid-template-rows: auto auto;
+  grid-template-rows: auto auto auto;
   align-content: space-between;
 `;
 
