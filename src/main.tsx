2feb397b210cb51a0f8afa71e06703c599a41df3
Salem Fenn <salemlfenn@gmail.com>
2024-06-19 10:50:06 -0700

    App file updated to use tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Render the app
+const rootElement = document.getElementById("app")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
