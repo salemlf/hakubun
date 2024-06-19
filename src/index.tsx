bb7e9eb888b0eeade07f4e3803f431864fe17448
Salem Fenn <salemlfenn@gmail.com>
2024-06-19 10:44:27 -0700

    Changed id of root element to 'app'

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
+const container = document.getElementById("app");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
