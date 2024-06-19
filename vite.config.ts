18564c6b82efd34f6c59e3bbdf74e6b21576ffae
Salem Fenn <salemlfenn@gmail.com>
2024-06-19 10:15:26 -0700

    Using tanstack vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import svgr from "vite-plugin-svgr";
+
+// vitest automatically sets NODE_ENV to 'test' when running tests
+const isTestEnv = process.env.NODE_ENV === "test";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,6 +29,7 @@ export default defineConfig({
         ],
       },
     }),
+    !isTestEnv && TanStackRouterVite(),
     svgr({ svgrOptions: { icon: true } }),
     sentryVitePlugin({
       org: "salem-fenn-1cdc31184",
