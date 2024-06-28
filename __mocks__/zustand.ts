eacced671ca046d83ed58426d54d5cbde20d7c31
Salem Fenn <salemlfenn@gmail.com>
2024-06-28 12:50:24 -0700

    Removed console.logs from zustand mocks

--- __mocks__/zustand.ts
+++ __mocks__/zustand.ts
@@ -19,8 +19,6 @@ const createUncurried = <T>(stateCreator: zustand.StateCreator<T>) => {
 
 // when creating a store, we get its initial state, create a reset function and add it in the set
 export const create = (<T>(stateCreator: zustand.StateCreator<T>) => {
-  console.log("zustand create mock");
-
   // to support curried version of create
   return typeof stateCreator === "function"
     ? createUncurried(stateCreator)
@@ -38,8 +36,6 @@ const createStoreUncurried = <T>(stateCreator: zustand.StateCreator<T>) => {
 
 // when creating a store, we get its initial state, create a reset function and add it in the set
 export const createStore = (<T>(stateCreator: zustand.StateCreator<T>) => {
-  console.log("zustand createStore mock");
-
   // to support curried version of createStore
   return typeof stateCreator === "function"
     ? createStoreUncurried(stateCreator)
