6db4a022561f7e23e79a50644220eb419b3b8c0e
Salem Fenn <salemlfenn@gmail.com>
2024-10-26 12:41:55 -0700

    In process of adding lesson quiz tests

--- src/pages/LessonQuiz.test.tsx
+++ src/pages/LessonQuiz.test.tsx
@@ -1,9 +1,61 @@
-import { act, createTestRouter, TestRoute } from "../testing/test-utils";
+import useAssignmentQueueStoreFacade from "../stores/useAssignmentQueueStore/useAssignmentQueueStore.facade";
+import useQueueStoreFacade from "../stores/useQueueStore/useQueueStore.facade";
+import { generateRandomQueueItems } from "../testing/mocks/data-generators/assignmentQueueGenerator";
+import {
+  act,
+  createTestRouter,
+  renderHook,
+  TestRoute,
+  screen,
+} from "../testing/test-utils";
+import { AssignmentQueueItem } from "../types/AssignmentQueueTypes";
 import LessonQuiz from "./LessonQuiz";
 
+const mockQueueStore = () => {
+  renderHook(() => useQueueStoreFacade());
+};
+
+const mockLessonQueueStore = (queueItems: AssignmentQueueItem[]) => {
+  const { result: assignmentQueueResult } = renderHook(() =>
+    useAssignmentQueueStoreFacade()
+  );
+  expect(assignmentQueueResult.current.assignmentQueue).toEqual([]);
+  act(() =>
+    assignmentQueueResult.current.setAssignmentQueueData(queueItems, "lesson")
+  );
+};
 test("LessonQuiz renders", async () => {
-  const { baseElement } = await renderComponent(false);
+  const { baseElement } = await renderComponent();
   expect(baseElement).toBeDefined();
+});
+
+describe("End quiz dialog", () => {
+  // TODO: doesn't work, fix
+  test("Displays dialog on home button click", async () => {
+    mockQueueStore();
+    const randomQueueItems = generateRandomQueueItems({
+      numItems: 10,
+      areLessons: true,
+      queueProgressState: "not_started",
+    });
+    mockLessonQueueStore(randomQueueItems);
+
+    const { user } = await renderComponent(true);
+
+    await user.click(
+      await screen.findByRole("button", {
+        name: /home page/i,
+      })
+    );
+
+    const dialog = await screen.findByRole("alertdialog", {
+      name: /end lesson quiz\?/i,
+    });
+
+    expect(dialog).toBeInTheDocument();
+
+    // screen.debug();
+  });
 });
 
 const renderComponent = async (withHomeRoute: boolean = false) => {
